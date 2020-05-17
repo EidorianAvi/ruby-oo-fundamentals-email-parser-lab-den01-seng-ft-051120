fc32145cde13af2fa1bad8e9df2e67657ad9ba6b
Rene Avila <reneavila1993@gmail.com>
2020-05-17 17:57:36 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -16,7 +16,7 @@ class EmailAddressParser
   
   def parse
     email_addresses.split.collect do |email_address|
-      email_address.strip
+      email_address.gsub(',','')
     end
     # binding.pry
   end
