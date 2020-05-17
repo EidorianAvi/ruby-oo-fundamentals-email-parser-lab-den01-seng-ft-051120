cd740a27cafe124a03a45460275db2d182f0091b
Rene Avila <reneavila1993@gmail.com>
2020-05-17 17:43:00 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,8 +7,8 @@ class EmailAddressParser
 
   attr_accessor :email_addresses
   
-  def parse emails 
-    emails.parse(',')
+  def parse email_addresses 
+    @emails_addresses.split(',')
   end
 
 
